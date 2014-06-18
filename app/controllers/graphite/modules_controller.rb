cb44f54b17142b236cbf8b4a3b76e66af58a32c3
blelump <blelump@martex.nom.pl>
2014-06-18 18:42:36 +0200

    WIP: Introduce elective modules CRUD
    Change-Id: I5f53c80bca21abaf79a8ac6157f9d2c67e1f26ba

---
+++ app/controllers/graphite/modules_controller.rb
@@ -0,0 +1,62 @@
+class Graphite::ModulesController < GraphiteController
+
+  authorize_resource :class => "Graphite::ElectiveBlock::ElectiveModule"
+
+  def create
+    @module = Graphite::ElectiveBlock::ElectiveModule.create(module_params)
+    preload
+    respond_to do |f|
+      f.js do
+        render layout: false
+      end
+    end
+  end
+
+  def edit
+    @module = Graphite::ElectiveBlock::ElectiveModule.find(params[:id])
+    preload
+    respond_to do |f|
+      f.js do
+        render layout: false
+      end
+    end
+  end
+
+  def update
+    @module = Graphite::ElectiveBlock::ElectiveModule.find(params[:id])
+    @module.update(module_params)
+    preload
+    respond_to do |f|
+      f.js do
+        render layout: false
+      end
+    end
+  end
+
+  def destroy
+    old_mod = Graphite::ElectiveBlock::ElectiveModule.find(params[:id])
+    @module = old_mod.elective_block.modules.build
+    old_mod.destroy
+    preload
+    respond_to do |f|
+      f.js do
+        render layout: false
+      end
+    end
+  end
+
+  private
+
+  def module_params
+    params.require(:elective_block).permit(:modules_attributes => [:id, :name,
+        :www, :owner_id, :student_amount, :ects_amount, :semester_number, :elective_block_id])
+    .values.first.values.first
+  end
+
+  def preload
+    @elective_block = Graphite::ElectiveBlock
+    .includes(:modules => [:translations, :employee => :employee_title])
+    .find(params[:elective_block_id])
+    @modules = @elective_block.modules.sort
+  end
+end
--- app/models/graphite/elective_block/elective_module.rb
+++ app/models/graphite/elective_block/elective_module.rb
@@ -4,5 +4,6 @@ class Graphite::ElectiveBlock::ElectiveModule < ActiveRecord::Base
   globalize_accessors :locales => I18n.available_locales
 
   belongs_to :elective_block
+  belongs_to :employee, foreign_key: :owner_id
 
 end
