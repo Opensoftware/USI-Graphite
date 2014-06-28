a9e5f135655d83b81d38d46aeb5fc823dfbcc8c7
blelump <blelump@martex.nom.pl>
2014-06-28 20:51:41 +0200

    Add dependent assets to precompile list array
    Change-Id: Ie6c180037d36832bdda4d24460e0f3cece2b8da9

--- lib/graphite/engine.rb
+++ lib/graphite/engine.rb
@@ -4,5 +4,11 @@ require 'graphite/enrollment/elective_block_n_from_m_subject.rb'
 module Graphite
   class Engine < ::Rails::Engine
     isolate_namespace Graphite
+
+    initializer "graphite.assets.precompile" do |app|
+      app.config.assets.precompile += %w(graphite/edit_elective_block.js graphite/elective_block.js
+graphite/elective_block_list.js graphite/new_elective_block.js graphite/pipe.js
+graphite/show_elective_block.js)
+    end
   end
 end
